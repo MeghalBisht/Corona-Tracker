ad0eb7a9bf09b7129fcd9671e4671ba7f147009f
Meghal <mailtomeghu25@gmail.com>
2020-06-02 23:33:13 +0530

    Updates

---
+++ precache-manifest.774f3600dab033e5660afd9a5705cdc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe31408bc81dbd70f1f9c54570e4ac7a",
+    "url": "/Corona-Tracker/index.html"
+  },
+  {
+    "revision": "e13f9e3a8aecc094d5d2",
+    "url": "/Corona-Tracker/static/css/main.21a61e18.chunk.css"
+  },
+  {
+    "revision": "075934b3778cdd93462c",
+    "url": "/Corona-Tracker/static/js/2.a65dba48.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/Corona-Tracker/static/js/2.a65dba48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13f9e3a8aecc094d5d2",
+    "url": "/Corona-Tracker/static/js/main.a01811af.chunk.js"
+  },
+  {
+    "revision": "d193bd2dbe7c985f698c",
+    "url": "/Corona-Tracker/static/js/runtime-main.00edccc4.js"
+  },
+  {
+    "revision": "47a4c9f242095eff1ec314c82cf02680",
+    "url": "/Corona-Tracker/static/media/corona.47a4c9f2.png"
+  }
+]);
